b6134e739692a4545ca8e80a14062adb56df5e0c
xmilygit <xmilygame@163.com>
2020-06-29 23:30:41 +0800

    毕业生登记表模板

---
+++ src/components/graduate/graduatetable.vue
@@ -0,0 +1,91 @@
+<template>
+    <div id="app">
+  <v-app id="inspire">
+    <v-main>
+      <v-container>
+        <div style="text-align:center">
+        <h2 style="margin-bottom:30px">2020年桂林市小学毕业生登记表</h2>
+       <table style="margin:auto">
+         <tr>
+         <td>
+           <span style="text-decoration:underline;margin-right:5px;">
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;叠彩&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           </span>
+           区
+         </td>
+         <td>
+           <span style="text-decoration:underline;margin-right:5px;margin-left:5px;">
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+             桂林市凤集
+             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           </span>
+           小学</td>
+           <td>
+             <span style="margin-left:5px;">学号:</span>
+             <span style="text-decoration:underline;margin-right:5px;">
+               &nbsp;G450205198008141012&nbsp;
+             </span>
+           </td><td>序号:<span style="text-decoration:underline;margin-right:5px;margin-left:5px;">
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           001
+           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+           </span></td>
+         </tr>
+        </table>
+       <table border="0" cellspacing="1" cellpadding="0" bgcolor="black" style="margin:auto">
+         <tr bgcolor="#fff" height="45" align="center">
+           <td width="70">姓名</td>
+           <td width="100">xxx</td>
+           <td width="40">性别</td>
+           <td width="40">xxx</td>
+           <td width="40">民族</td>
+           <td width="60">xx</td>
+           <td width="40">出生年月</td>
+           <td>xx</td>
+           <td rowspan="4" width="150">照片</td>
+         </tr>
+         <tr bgcolor="#fff" height="45" align="center">
+         <td>是否队员</td>
+           <td>xx</td>
+         <td>担任职务</td>
+           <td colspan="2">xx</td>
+         <td>健康<br/>状况</td>
+           <td colspan="2">xxx</td>
+         </tr>
+         <tr bgcolor="#fff" height="45" align="center">
+           <td>父亲姓名</td>
+           <td>XX</td>
+           <td rowspan="2">工作单位</td>
+           <td colspan="3">XX</td>
+           <td>联系电话</td>
+           <td>XX</td>
+         </tr>
+         <tr bgcolor="#fff" height="45" align="center">
+           <td>父亲姓名</td>
+           <td>XX</td>           
+           <td colspan="3">XX</td>
+           <td>联系电话</td>
+           <td>XX</td>
+         </tr>
+         <tr bgcolor="#fff" height="45" align="center">
+           <td>户口<br/>所在地址</td>
+           <td>xxx</td>
+           <td>户口性质</td>
+           <td colspan="2">xxx</td>
+           <td colspan="2">家庭详细住址及联系电话</td>
+           <td colspan="2">xxx</td>
+         </tr>
+        </table>
+                  </div>
+       </v-container>
+      </v-main>
+  </v-app>
+</div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
